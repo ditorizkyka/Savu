00ffd6a3e22da203b2dcfbf24ce209ada3082e61
ditorizkyka <rizkykandito@gmail.com>
2026-03-11 13:52:44 +0700

    Initial Commit

---
+++ savuapp/savuappApp.swift
@@ -0,0 +1,17 @@
+//
+//  savuappApp.swift
+//  savuapp
+//
+//  Created by Andito Rizkyka Rianto on 11/03/26.
+//
+
+import SwiftUI
+
+@main
+struct savuappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
